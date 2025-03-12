346523fc2b28de814a974902a27781d1af7a5004
Anastasiia Nikonova <anastaney@gmail.com>
2025-03-12 08:36:19 +0100

    feat: implement getCountDaysInMonth

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -37,10 +37,9 @@ function formatTime(unit) {
 }
 
 function getTime(date) {
-  const target = new Date(date);
-  const hours = formatTime(target.getHours());
-  const minutes = formatTime(target.getMinutes());
-  const seconds = formatTime(target.getSeconds());
+  const hours = formatTime(date.getHours());
+  const minutes = formatTime(date.getMinutes());
+  const seconds = formatTime(date.getSeconds());
   return `${hours}:${minutes}:${seconds}`;
 }
 
@@ -98,8 +97,8 @@ function getNextFriday(/* date */) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
 }
 
 /**
